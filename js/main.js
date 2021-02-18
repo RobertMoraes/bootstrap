066d7aff4af3032bdca91e27b8d864730157d6c2
RobertMoraes <robert.moraes@outlook.com>
2021-02-17 23:22:16 -0300

    Atualização das imagens com onclick

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function redLink(){
+    window.open("https://www.linkedin.com/in/robertmoraespereira/");
+}
+
+function redGit(){
+    window.open("https://github.com/RobertMoraes");
+}
+
+function redInsta(){
+    window.open("https://www.instagram.com/robert_social/");
+}
